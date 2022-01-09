8883ab676f84b7cc80ee4f4ba9271f010630b4c4
Sontus Chandra Anik <info.sontus@gmail.com>
2022-01-09 09:42:41 +0600

    FAQ Page Done

--- src/compontents/Pages/Faq/Faq.js
+++ src/compontents/Pages/Faq/Faq.js
@@ -3,7 +3,202 @@ import React from 'react';
 const Faq = () => {
     return (
         <>
-            
+        <div className="header-height"></div>
+        
+        <section className="page-header padding">
+            <div className="anim-elements">
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+            </div>
+		    <div className="container">
+		        <div className="page-content text-center">
+		            <h2>Frequently Asked Questions!</h2>
+		            <p>Multipurpose Saas Startup HTML Template.</p>
+		        </div>
+		    </div>
+		</section>
+       
+        <section className="faq-section bd-bottom padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 padding-15">
+                        <div id="accordion" className="faq-item">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link" data-toggle="collapse" data-target="#faq1" aria-expanded="true" aria-controls="faq1">
+                                          What's the difference between 32-bit and 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq1" className="collapse show" aria-labelledby="faq1" data-parent="#accordion">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq2" aria-expanded="false" aria-controls="faq2">
+                                          My Windows 10 product key won't verify. What's the problem?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq2" className="collapse" aria-labelledby="faq2" data-parent="#accordion">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                           <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq3" aria-expanded="false" aria-controls="faq3">
+                                          How do I tell if my computer can run a 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq3" className="collapse" aria-labelledby="faq3" data-parent="#accordion">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                           </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 padding-15">
+                        <div id="accordion2" className="faq-item">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link" data-toggle="collapse" data-target="#faq4" aria-expanded="true" aria-controls="faq4">
+                                          What's the difference between 32-bit and 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq4" className="collapse show" aria-labelledby="faq4" data-parent="#accordion2">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq5" aria-expanded="false" aria-controls="faq5">
+                                          My Windows 10 product key won't verify. What's the problem?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq5" className="collapse" aria-labelledby="faq5" data-parent="#accordion2">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                           <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq6" aria-expanded="false" aria-controls="faq6">
+                                          How do I tell if my computer can run a 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq6" className="collapse" aria-labelledby="faq3" data-parent="#accordion2">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                           </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 padding-15">
+                        <div id="accordion3" className="faq-item">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link" data-toggle="collapse" data-target="#faq7" aria-expanded="true" aria-controls="faq7">
+                                          What's the difference between 32-bit and 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq7" className="collapse show" aria-labelledby="faq7" data-parent="#accordion3">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq8" aria-expanded="false" aria-controls="faq8">
+                                          My Windows 10 product key won't verify. What's the problem?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq8" className="collapse" aria-labelledby="faq8" data-parent="#accordion3">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                           <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq9" aria-expanded="false" aria-controls="faq9">
+                                          How do I tell if my computer can run a 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq9" className="collapse" aria-labelledby="faq9" data-parent="#accordion3">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                           </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 padding-15">
+                        <div id="accordion4" className="faq-item">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link" data-toggle="collapse" data-target="#faq10" aria-expanded="true" aria-controls="faq10">
+                                          What's the difference between 32-bit and 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq10" className="collapse show" aria-labelledby="faq10" data-parent="#accordion4">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq11" aria-expanded="false" aria-controls="faq11">
+                                          My Windows 10 product key won't verify. What's the problem?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq11" className="collapse" aria-labelledby="faq11" data-parent="#accordion4">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                            </div>
+                           <div className="card">
+                                <div className="card-header">
+                                    <h5 className="mb-0">
+                                        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#faq12" aria-expanded="false" aria-controls="faq12">
+                                          How do I tell if my computer can run a 64-bit Windows?
+                                        </button>
+                                    </h5>
+                                </div>
+                                <div id="faq12" className="collapse" aria-labelledby="faq12" data-parent="#accordion4">
+                                    <div className="card-body">We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents.
+                                    </div>
+                                </div>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     );
 };
