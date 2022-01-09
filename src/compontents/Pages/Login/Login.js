36ee86ee7335c9e326ccbc51be8ad6c243f0f043
Sontus Chandra Anik <info.sontus@gmail.com>
2022-01-09 09:37:14 +0600

    Login Page Done

--- src/compontents/Pages/Login/Login.js
+++ src/compontents/Pages/Login/Login.js
@@ -3,7 +3,30 @@ import React from 'react';
 const Login = () => {
     return (
         <>
-            
+        <section className="login-section">
+           <div className="map"></div>
+            <div className="container">
+                <div className="row d-flex align-items-center">
+                    <div className="col-lg-4 offset-lg-4">
+                        <div className="login-box">
+                            <div className="section-heading mb-30">
+                                <h2>Login To Your Account</h2>
+                                <p>Multipurpose Saas Startup Template.</p>
+                            </div>
+                            <form className="login-form">
+                                <div className="form-group">
+                                    <input type="email" className="form-control" id="email" aria-describedby="email" placeholder="Email address" required=""/>
+                                </div>
+                                <div className="form-group">
+                                    <input type="password" className="form-control" id="password" placeholder="Password" required=""/>
+                                </div>
+                                <button type="submit" className="default-btn btn-blue">Login Now</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     );
 };
