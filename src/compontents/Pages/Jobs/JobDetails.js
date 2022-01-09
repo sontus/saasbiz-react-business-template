58e030eb978da129fd92236adf5a004bdf1602ca
Sontus Chandra Anik <info.sontus@gmail.com>
2022-01-09 09:30:20 +0600

    Job Details Page Done

--- src/compontents/Pages/Jobs/JobDetails.js
+++ src/compontents/Pages/Jobs/JobDetails.js
@@ -3,7 +3,71 @@ import React from 'react';
 const JobDetails = () => {
     return (
         <>
-            
+        <div className="header-height"></div>
+        <section className="page-header padding">
+            <div className="anim-elements">
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+            </div>
+		    <div className="container">
+		        <div className="page-content text-center">
+		            <h2>Digital Marketer<br/> Web & Content Specialist</h2>
+		            <p>Multipurpose Saas Startup HTML Template.</p>
+		        </div>
+		    </div>
+		</section>
+       
+        <section className="job-details-section padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 sm-padding">
+                        <div className="job-details-wrap">
+                            <h2>Job Requirements:</h2>
+                            <p>We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents. Provide marketing services to startups and small businesses to looking for a partner of their digital media.</p>
+                        </div>
+                        <div className="row case-list-wrap mb-40">
+                            <div className="col-md-6 xs-padding">
+                                <ul className="case-list">
+                                    <li><i className="fas fa-thumbs-up"></i>Installation of final floor covering, such as floor tile, carpet, or wood flooring.</li>
+                                    <li><i className="fas fa-thumbs-up"></i>Building inspector visits if necessary to approve utilities and framing.</li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 xs-padding">
+                                <ul className="case-list">
+                                    <li><i className="fas fa-thumbs-up"></i>If required obtain approval from HOA or ARC architectural review committee.</li>
+                                    <li><i className="fas fa-thumbs-up"></i>Cover outer walls and roof in OSB or plywood and a water-restive more barrier.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="job-details-wrap">
+                            <h2>Company Rules & Regulations:</h2>
+                            <p>We provide marketing services to startups and small businesses to looking for a partner of their digital media, design & development, lead generation and communications requirents. Provide marketing services to startups and small businesses to looking for a partner of their digital media.</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4 sm-padding">
+                        <div className="job-details-sidebar">
+                            <div className="case-sidebar-item">
+                                <h2>Job Details</h2>
+                                <ul className="case-sidebar-list">
+                                    <li><span>Department:</span>Senior Web Designer</li>
+                                    <li><span>Location:</span>962 Fifth Avenue, 3rd Floor New York, NY10022</li>
+                                    <li><span>Job Type:</span>Planning</li>
+                                    <li><span>Qualifications:</span>Associate</li>
+                                    <li><span>Experience:</span>Minimum 5 Years</li>
+                                    <li><span>Offered Salary:</span>450,000</li>
+                                </ul>
+                                <div className="job-details-wrap">
+                                    <a href="#" className="default-btn btn-blue">Apply Job Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     );
 };
