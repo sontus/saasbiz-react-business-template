5e24d46cb494ac2316b0953cf25ae5a8af2d3eea
Sontus Chandra Anik <info.sontus@gmail.com>
2022-01-09 09:32:18 +0600

    Our Team Page Done

--- src/compontents/Pages/Team/Team.js
+++ src/compontents/Pages/Team/Team.js
@@ -3,7 +3,166 @@ import React from 'react';
 const Team = () => {
     return (
         <>
-            
+        <div className="header-height"></div>
+        <section className="page-header padding">
+            <div className="anim-elements">
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+                <div className="anim-element"></div>
+            </div>
+		    <div className="container">
+		        <div className="page-content text-center">
+		            <h4>Team Members</h4>
+                    <h2>Our Expert Team Would Like <br/> To Hear From You!</h2>
+		            <p>Multipurpose Saas Startup HTML Template.</p>
+		        </div>
+		    </div>
+		</section>
+        
+        <section className="tophic-team bd-bottom padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                       <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-1.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Fiorella Ibáñez<span>Developer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                       <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-2.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>José Carpio<span>Developer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                       <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-3.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Jhon Castellon<span>Developer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                        <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-4.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Kyle Frederick<span>Developer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                        <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-5.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Eldie Goldey<span>Marketer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                        <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-6.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Tylor Swift<span>Designer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                        <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-7.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Justin Beiber<span>Developer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-6 padding-15">
+                        <div className="team-wrap">
+                           <div className="team-box">
+                                <div className="overlay"></div>
+                                <img src="assets/img/team-8.jpg" alt="img"/>
+                                <ul className="team-social">
+                                    <li><a href="#"><i className="ti-facebook"></i></a></li>
+                                    <li><a href="#"><i className="ti-twitter"></i></a></li>
+                                    <li><a href="#"><i className="ti-instagram"></i></a></li>
+                                    <li><a href="#"><i className="ti-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                            <div className="team-content">
+                                <h3>Cristiano Messi<span>Designer</span></h3>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     );
 };
